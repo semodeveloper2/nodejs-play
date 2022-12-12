cc1b83c2f8afb1aacae8297cfd0efa7fa6af6a67
semo developer <semodeveloper@gmail.com>
2022-12-12 22:32:02 +0000

    Update 5 files

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -152,6 +152,8 @@ array1 = regex1.exec(str1);
 console.log("Found ".concat(array1[0], ". Next starts at ").concat(regex1.lastIndex, "."));
 console.log(regex1, array1[0].length);
 console.log(str1[regex1.lastIndex - array1[0].length], str1[regex1.lastIndex - 1]);
+
+/////////////////////////////////////
 },{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -177,7 +179,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "37317" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "45901" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
