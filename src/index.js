849a3a6c563ef6486260a93fe935c1440f42a7b3
semo developer <semodeveloper@gmail.com>
2022-12-12 22:18:51 +0000

    Update 8 files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-const regex1 = RegExp("foo*", "g");
-const str1 = "table football, foosball";
+var regex1 = RegExp("foo*", "g");
+var str1 = "table football, foosball";
 let array1 = regex1.exec(str1);
 
 while (array1 !== null) {
@@ -13,3 +13,21 @@ let str = "I love JavaScript";
 let result = str.match(/Java(Script)/);
 console.log(result);
 console.log({ a: 1, b: 2, c: 3, d: 4, eeeee: 565, ffffff: 66666 });
+
+/////////////////////////////////////////////////////////////////////////
+
+regex1 = RegExp("foo*", "g");
+str1 = "table foootball, foosball";
+//let array1;
+
+array1 = regex1.exec(str1);
+console.log(`Found ${array1[0]}. Next starts at ${regex1.lastIndex}.`);
+
+array1 = regex1.exec(str1);
+console.log(`Found ${array1[0]}. Next starts at ${regex1.lastIndex}.`);
+
+console.log(regex1, array1[0].length);
+console.log(
+  str1[regex1.lastIndex - array1[0].length],
+  str1[regex1.lastIndex - 1]
+);
